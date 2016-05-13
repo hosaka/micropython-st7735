d7861fa887cc545b13f7ceb28e4bff1a4d1bc401
Alex March <alex.march.dev@gmail.com>
2016-05-13 01:52:33 +0100

    rfc: Remove license strings.

--- st7735.py
+++ st7735.py
@@ -1,27 +1,3 @@
-"""
-The MIT License (MIT)
-
-Copyright (c) 2016 Alex March (github.com/hosaka)
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE
-"""
-
 # MicroPython ST7735 TFT display driver
 
 class ST7735(object):
@@ -188,12 +164,6 @@ class ST7735(object):
         # font?
         pass
 
-    """
-    ST7735 HAL functions
-
-    Implement these in your driver API to communicate with the display using
-    defined Pins and SPI bus.
-    """
     def init(self):
         """
         HAL: Init your "tab" version of the display.
